a76457cd8fd01a6ca9d4bf3da096f6786563f1d1
Malthe <grom1020@gmail.com>
2025-12-05 13:59:29 +0100

    NewTyper now checks for duplicate class definitions.

--- choral/src/main/java/choral/compiler/NewTyper.java
+++ choral/src/main/java/choral/compiler/NewTyper.java
@@ -1,11 +1,19 @@
 package choral.compiler;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 
 import choral.ast.CompilationUnit;
 import choral.ast.type.FormalWorldParameter;
+import choral.exceptions.AstPositionedException;
+import choral.exceptions.StaticVerificationException;
 
 public class NewTyper {
+    // Does this hashmap need to be threadsafe?
+    private static final HashMap<String, List<String>> classToRolesMap = new HashMap<>();
+
     public static Collection<CompilationUnit> annotate(Collection<CompilationUnit> sourceUnits){
         Visitor visitor = new Visitor();
         sourceUnits.forEach( cu -> visitor.visit(cu));
@@ -20,11 +28,18 @@ public class NewTyper {
         }
 
         private void visitClass(choral.ast.body.Class visitedClass){
-            System.out.println("Found class: " + visitedClass.name().identifier());
-            System.out.println("With role(s):");
-            for (FormalWorldParameter param : visitedClass.worldParameters()){
-                System.out.println(param.name().identifier());
-            }
+            if (!classToRolesMap.containsKey(visitedClass.name().identifier())){
+                List<String> roles = new ArrayList<>();
+                for (FormalWorldParameter param : visitedClass.worldParameters()){
+                    roles.add(param.name().identifier());
+                }
+                classToRolesMap.put(visitedClass.name().identifier(), roles);
+            } else {
+                throw new AstPositionedException(visitedClass.position(), 
+                        new StaticVerificationException("Duplicate class definition: " + 
+                            visitedClass.name().identifier()
+                            + " already defined."));
+            }           
         }
     }
 }
